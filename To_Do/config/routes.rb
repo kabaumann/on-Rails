9d2726376853bf15a57b04cda99c8c13dcd58d3e
kabaumann <kellyabaumann@gmail.com>
2015-02-12 08:48:13 -0600

    added users to To Do list.

--- To_Do/config/routes.rb
+++ To_Do/config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  resources :users
+
   resources :todos
 
-  root to: 'todos#create'
+  root 'todos#create'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
