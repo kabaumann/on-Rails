3330623be233d6b94405a36fdb3406146e923a5f
kabaumann <kellyabaumann@gmail.com>
2015-02-24 09:51:46 -0600

    plus footer

--- home_splash/app/controllers/home_controller.rb
+++ home_splash/app/controllers/home_controller.rb
@@ -1,19 +1,21 @@
 class HomeController < ApplicationController
-  def index
-  right_now = Time.now
-  @current_time = right_now.to_formatted_s(:short)
-  @greet_message
+def index
+right_now = Time.now
+@current_time = right_now.stamp ("10:30 am")
+date_now = Date.today
+@current_date = date_now.stamp ("Mon Feb 23, 2015")
+@greet_message
 #homework: add time stamp
 #Adjust message by time of day
-  @greet_message = case Time.now.hour
-  when 5..11
-    "Guten Morgen!"
-  when 12..15
-    "Gute Mittag!"
-  when 16..19
-    "Guten Abend!"
-  when 20..23, 0..4
-    "Gute Nacht!"
-    end
-  end
+@greet_message = case Time.now.hour
+when 5..11
+"Guten Morgen!"
+when 12..15
+"Gute Mittag!"
+when 16..19
+"Guten Abend!"
+when 20..23, 0..4
+"Gute Nacht!"
+end
+end
 end
